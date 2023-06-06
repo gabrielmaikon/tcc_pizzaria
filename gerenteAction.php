3f417b8c5d977c8f2fe536268ce66023a4fb5c1b
Gabriel <gabrielmaikon07@gmail.com>
2023-06-06 00:41:56 -0300

    Inserindo logs para criação do funcionário

--- gerenteAction.php
+++ gerenteAction.php
@@ -1,6 +1,20 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\SocketHandler;
+use Monolog\Formatter\JsonFormatter;
+
 require_once('conexaoBD.php');
+
+// Crie uma instância do logger
+$logger = new Logger('pizzaria');
+
+// Configure o SocketHandler para enviar logs para o Logstash
+$socketHandler = new SocketHandler('tcp://localhost:5044');
+$socketHandler->setFormatter(new JsonFormatter());
+$logger->pushHandler($socketHandler);
 
 $nomeFunc = $_POST['txtNome'];
 $emailFunc = $_POST['txtEmail'];
@@ -14,20 +28,23 @@ $cargoFunc = $_POST['selectCargo'];
 //-----------------------------------
 
 $sql = "INSERT INTO usuario (nome, email, senha, data_nasc, endereco, telefone, celular, cpf, cargo_id) VALUES ('" . $nomeFunc . "', '" . $emailFunc . "', '" . $senhaFunc . "', '" . $dataNascFunc . "', '" . $enderecoFunc . "', '" . $telefoneFunc . "', '" . $celularFunc . "', '" . $cpfFunc . "', " . $cargoFunc . ")";
-//exit(var_dump($sql));
+
 if ($conexao->query($sql)) {
     echo '
        <a href="gerente.php">
             <h1 class="w3-button w3-teal w3-display-middle"> Salvo com sucesso! </h1>
         </a>
              ';
+    $logger->info('Funcionário inserido com sucesso!');
 } else {
     echo '
         <a href="gerente.php">
            <h1 class="w3-button w3-teal w3-display-middle" style="background-color: red !important">ERRO! </h1>
        </a>
            ';
+    $logger->info('Erro ao inserir funcionário!');
 }
+
 ?>
 <style>
     #btn1 {
